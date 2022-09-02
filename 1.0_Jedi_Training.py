96cc005dfd1cdc723411bb2880943ed9aa6c49bc
huynhlin <huynhlin@urbandaleschools.com>
2022-09-02 08:46:28 -0500

    a

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Lindy Huynh
 
 
 
-Define Forking: 
+Define Forking: taking someone else's code and taking it onto your own profile
 
-Define Cloning: 
+Define Cloning: downloading the forked code to your own device
 
-Define Branching:
+Define Branching: another copy of your code where you can make changes but still have the original unedited master branch to fall back on in case you dont like the branch code you made
 
-Define Committing: 
+Define Committing: making a big save kind of
 
-Define Merging: 
+Define Merging: merging a branch of your code into the master branch
 
-Define Pushing:
+Define Pushing: updating the code on your github page to match the one on your device
 
-Define Pull Request:
+Define Pull Request: asking the owner of the forked code you made to pull your updates into the their code
 
 
 '''
