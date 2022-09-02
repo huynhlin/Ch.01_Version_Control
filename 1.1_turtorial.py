77c129ae46d623748eecfc4f78a736b401d4f5f2
huynhlin <huynhlin@urbandaleschools.com>
2022-09-02 09:10:40 -0500

    my battery is dying yo

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -8,28 +8,22 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
+tyson=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+tyson.pensize(3) # width of pen line
+tyson.speed(10)  # speed of drawing. Go fast to not waste time.
+tyson.color("#ffffff")
+tyson.home()
+tyson.circle(50)
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+
+tyson.penup()
+tyson.setpos(200,-300)
+tyson.down()
+tyson.pencolor('#ffffff')
+
+
+tyson.write('Lindy Huynh',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
